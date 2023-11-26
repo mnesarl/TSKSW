135a00f5411e1de7ea025ab58dc43d4c444f1d2a
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-11-26 14:31:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,19 @@
 import subprocess
 
-def get_wifi_profiles():
-  """
-  Mendapatkan daftar profil WiFi yang pernah terhubung.
+output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout.decode("utf-8")
 
-  Returns:
-    List of dictionary, berisi informasi profil WiFi.
-  """
+profiles = []
 
-  output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout.decode("utf-8")
-  profiles = []
-  for line in output.splitlines():
+for line in output.splitlines():
     if "Profile" in line:
-      profile = {}
-      profile["name"] = line.split(":")[1].strip()
-      profiles.append(profile)
-  return profiles
+        profile = {}
+        profile["name"] = line.split(":")[1].strip()
+        profiles.append(profile)
 
-def get_wifi_password(profile_name):
-  """
-  Mendapatkan password WiFi dari profil tertentu.
-
-  Args:
-    profile_name: Nama profil WiFi.
-
-  Returns:
-    Password WiFi, jika ada.
-  """
-
-  output = subprocess.run(["netsh", "wlan", "show", "profiles", profile_name, "key=clear"], capture_output=True).stdout.decode("utf-8")
-  for line in output.splitlines():
-    if "Key Content" in line:
-      return line.split(":")[1].strip()
-  return None
-
-def main():
-  """
-  Menjalankan program.
-  """
-
-  profiles = get_wifi_profiles()
-  for profile in profiles:
-    password = get_wifi_password(profile["name"])
-    if password:
-      print(f"{profile['name']}: {password}")
-
-if __name__ == "__main__":
-  main()
+for profile in profiles:
+    output = subprocess.run(["netsh", "wlan", "show", "profiles", profile["name"], "key=clear"], capture_output=True).stdout.decode("utf-8")
+    for line in output.splitlines():
+        if "Key Content" in line:
+            password = line.split(":")[1].strip()
+            if password:
+                print(f"{profile['name']}: {password}")
