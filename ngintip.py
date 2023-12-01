a578f6ee6661e976fbd9b8b2d372f6291d2c0778
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-01 11:57:26 +0700

    Update ngintip.py

--- ngintip.py
+++ ngintip.py
@@ -1,29 +1,39 @@
-import os
-import subprocess
+import os  # Mengimpor modul os untuk mengakses fungsi-fungsi sistem operasi
+import subprocess  # Mengimpor modul subprocess untuk menjalankan perintah subshell
 
-os.system("cls")
-output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout.decode("utf-8")
-profiles = []
+os.system("cls")  # Membersihkan layar konsol menggunakan perintah sistem "cls" pada Windows
 
-for line in output.splitlines():
-    if "All User Profile" in line:
-        profile = {}
-        profile["name"] = line.split(":")[1].strip()
-        profiles.append(profile)
+# Menjalankan perintah "netsh wlan show profiles" untuk menampilkan daftar profil Wi-Fi dan menyimpan keluarannya ke dalam variabel keluaran
+keluaran = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True).stdout.decode("utf-8")
 
-wifi_data = []
+profil_wifi = []  # Membuat list kosong untuk menyimpan informasi profil Wi-Fi
 
-for profile in profiles:
-    output = subprocess.run(["netsh", "wlan", "show", "profiles", profile["name"], "key=clear"], capture_output=True).stdout.decode("utf-8")
-    if "Key Content" in output:
-        wifi_info = {"Jaringan WI-FI": profile["name"], "Kata sandi": ""}
-        password = [line.split(":")[1].strip() for line in output.splitlines() if "Key Content" in line][0]
-        wifi_info["Kata sandi"] = password
-        wifi_data.append(wifi_info)
+# Memproses setiap baris dari keluaran perintah sebelumnya untuk mengidentifikasi nama-nama profil Wi-Fi
+for baris in keluaran.splitlines():
+    if "All User Profile" in baris:
+        profil = {}  # Membuat dictionary kosong untuk setiap profil Wi-Fi yang ditemukan
+        profil["nama"] = baris.split(":")[1].strip()  # Menyimpan nama profil Wi-Fi
+        profil_wifi.append(profil)  # Menambahkan informasi profil ke dalam list profil_wifi
 
+data_wifi = []  # Membuat list kosong untuk menyimpan informasi jaringan Wi-Fi beserta kata sandinya
+
+# Untuk setiap profil yang ditemukan, menjalankan perintah untuk mendapatkan informasi lebih lanjut tentang jaringan Wi-Fi
+for profil in profil_wifi:
+    keluaran = subprocess.run(["netsh", "wlan", "show", "profiles", profil["nama"], "key=clear"], capture_output=True).stdout.decode("utf-8")
+    if "Key Content" in keluaran:  # Memeriksa apakah informasi kata sandi Wi-Fi tersedia
+        info_wifi = {"Jaringan WI-FI": profil["nama"], "Kata sandi": ""}  # Membuat dictionary untuk menyimpan informasi jaringan Wi-Fi
+        sandi = [baris.split(":")[1].strip() for baris in keluaran.splitlines() if "Key Content" in baris][0]  # Mendapatkan kata sandi Wi-Fi
+        info_wifi["Kata sandi"] = sandi  # Menyimpan kata sandi Wi-Fi dalam dictionary info_wifi
+        data_wifi.append(info_wifi)  # Menambahkan informasi jaringan Wi-Fi ke dalam list data_wifi
+
+# Mencetak header tabel untuk informasi jaringan Wi-Fi dan kata sandi
 print("-" * 50)
 print("Jaringan WI-FI".ljust(25) + "Kata sandi")
 print("-" * 50)
-for data in wifi_data:
+
+# Mencetak informasi jaringan Wi-Fi dan kata sandi ke layar dengan format tabel yang rapi
+for data in data_wifi:
     print(data["Jaringan WI-FI"].ljust(25) + data["Kata sandi"])
+
+# Mencetak garis putus-putus sebagai pemisah setelah selesai menampilkan informasi
 print("-" * 50)
